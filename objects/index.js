5542cedc62bf315688228b4a91b8ab9ebab44502
lizkavalski <lizkavalski@DESKTOP-IM8BUGM.localdomain>
2019-06-27 20:44:50 -0700

    I think I'm done?

--- objects/index.js
+++ objects/index.js
@@ -2,6 +2,7 @@
 
 const VehicleConstructor = require('./vehicle-constructor.js');
 
+const VehicleClass= require ('./vehicle-class');
 // Implement a car and motorcycle using a Constructor
 const mazda = new VehicleConstructor.Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
@@ -10,4 +11,10 @@ const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
 // Implement a car and motorcycle using a Class
+
+let truck = new VehicleClass.Car('Big Truck');
+console.log(truck.name, truck.drive(),truck.stop());
+
+let bike = new VehicleClass.Motorcycle('Big Bike');
+console.log(bike.name, bike.wheelie(), bike.stop());
 // Your code here ....
